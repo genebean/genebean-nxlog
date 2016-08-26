c6e4b1d494fa86d5208e8ede4d770f6fdcda69ed
Camilo Cota <camilocot@gmail.com>
2016-08-26 23:00:04 +0200

    Added InputType for inputs (e.g. InputType 'multiline')

--- manifests/config/input.pp
+++ manifests/config/input.pp
@@ -12,12 +12,13 @@
 # Resulting output:
 #
 define nxlog::config::input (
-  $conf_dir     = $::nxlog::conf_dir,
-  $conf_file    = $::nxlog::conf_file,
-  $input_execs  = $::nxlog::input_execs,
+  $conf_dir        = $::nxlog::conf_dir,
+  $conf_file       = $::nxlog::conf_file,
+  $input_execs     = $::nxlog::input_execs,
   $input_file_path = $::nxlog::input_file_path,
-  $input_module = $::nxlog::input_module,
-  $order_input  = $::nxlog::order_input,) {
+  $input_module    = $::nxlog::input_module,
+  $input_type      = $::nxlog::input_type,
+  $order_input     = $::nxlog::order_input,) {
   concat::fragment { "input_${name}":
     target  => "${conf_dir}/${conf_file}",
     order   => $order_input,
